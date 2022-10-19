55d590d8205ab131afa47c89329a270928f7b991
bilalofkir <116163850+bilalofkir@users.noreply.github.com>
2022-10-19 13:16:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let text = document.getElementById("text")
+let num = -0
+
+function setAdd(){
+    num += 1
+    text.innerHTML = num
+}
+
+function setSub(){
+    num -= 1
+    text.innerHTML = num
+}
